d875423ee7473463a84f5d16d8a8dc40f285d2ee
Yashwanth Dahanayake <yd322@drexel.edu>
2017-05-22 22:28:49 -0400

    Updated Sheep Script.cs file

---
+++ Assets/Scripts/SheepScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepScript : MonoBehaviour
+{
+	public int sheepValue;
+
+	public void KillSheep()
+	{
+		if (Arithmetic.primaryArithmetic != null) {
+
+
+			Arithmetic.primaryArithmetic.PrintEquationOnScreen (sheepValue);
+		}
+
+		Destroy(gameObject);
+	}
+}
